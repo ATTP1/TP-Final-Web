fc82022064745d60dd04b2189829b5458c0315d7
ATTP1 <allantritampham@gmail.com>
2025-08-21 22:18:37 -0400

    Gestion animation link avec le bouton en mode mobile pour le navBar

--- js/navBar.js
+++ js/navBar.js
@@ -1,7 +1,25 @@
-gsap.from("#test1", {
-  y: 50, 
-  opacity: 0, 
-  duration: 1, 
-  ease: "power.out",
+const btn_navBarM = document.getElementById("navBar_M_animation");
+
+const navBar_sequenceAnimation = gsap.timeline({
+  paused: true,
+  reversed: true,
 });
 
+navBar_sequenceAnimation
+  .to("#iconBarMobile a", {
+    opacity: 0,
+    
+  })
+  .to(".navBar_M_animation", {
+    yPercent: 75,
+    duration: 1,
+    ease: "bounce.out",
+  });
+
+btn_navBarM.addEventListener("click", () => {
+  if (navBar_sequenceAnimation.reversed()) {
+    navBar_sequenceAnimation.play();
+  } else {
+    navBar_sequenceAnimation.reverse();
+  }
+});
