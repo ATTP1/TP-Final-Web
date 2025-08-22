315df0744656e1e299c80b0627633599ff020252
ATTP1 <allantritampham@gmail.com>
2025-08-22 03:30:40 -0400

    Ajout animation pour intro page info projet

--- js/projet_InfoBox.js
+++ js/projet_InfoBox.js
@@ -2,26 +2,33 @@ const carousel = document.getElementById("carousel__plusInfo");
 
 const btnFermer = document.getElementById("projetsInfoPlus__fermer");
 
+const projetsInfoPlus = document.getElementById("projetsInfoPlus");
 
+carousel.addEventListener("click", () => {  
 
-carousel.addEventListener("click", () => {
-  
-  const activeItem = carousel.querySelector('[data-carousel-item]:not(.hidden)');   
-  const box = activeItem.querySelector('.carousel_InfoBoite');
+  projetsInfoPlus.classList.remove("hidden");
 
-  const projetsInfoPlus = document.getElementById("projetsInfoPlus")
-  projetsInfoPlus.classList.remove("hidden")
-  
-  
+  gsap.from(projetsInfoPlus, {
+    y: -1000,
+    duration: 1.5,
+    opacity: 0.8,
+    ease: "bounce.out",
+  });
 });
 
-btnFermer.addEventListener("click", () => {
+btnFermer.addEventListener("click", () => {      
+
     
+  projetsInfoPlus.classList.add("hidden");
 
-  const projetsInfoPlus = document.getElementById("projetsInfoPlus")
-  projetsInfoPlus.classList.add("hidden")
-  
   
 });
 
+/*
 
+const activeItem = carousel.querySelector(
+    "[data-carousel-item]:not(.hidden)"
+  );
+  const box = activeItem.querySelector(".carousel_InfoBoite");
+
+*/
