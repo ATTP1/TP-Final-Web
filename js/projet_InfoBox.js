42a313d1b51c1d28d347177e3c2cac52cf2a9822
ATTP1 <allantritampham@gmail.com>
2025-08-24 02:43:06 -0400

    AJout lien avec le js pour lire l'item actif du carousel et l'appliquer au Id du la page cacher

--- js/projet_InfoBox.js
+++ js/projet_InfoBox.js
@@ -4,8 +4,7 @@ const btnFermer = document.getElementById("projetsInfoPlus__fermer");
 
 const projetsInfoPlus = document.getElementById("projetsInfoPlus");
 
-carousel.addEventListener("click", () => {  
-
+carousel.addEventListener("click", (projet) => {
   projetsInfoPlus.classList.remove("hidden");
 
   gsap.from(projetsInfoPlus, {
@@ -14,21 +13,54 @@ carousel.addEventListener("click", () => {
     opacity: 0.8,
     ease: "bounce.out",
   });
-});
 
-btnFermer.addEventListener("click", () => {      
+  const itemActif = projet.target.closest("[data-carousel-item]");
+  const boite = itemActif.querySelector(".carousel_InfoBoite");
+  const NomProjet = boite.querySelector("h3");
 
-    
-  projetsInfoPlus.classList.add("hidden");
+  const titreBoite = document.getElementById("projetsInfoPlus__titre");
+  
 
+  titreBoite.textContent = NomProjet.textContent.trim();
+
+  
+  const infoBoite = document.getElementById("projetsInfoPlus__info");
+
+  console.log(titreBoite.textContent)
+
+  switch (titreBoite.textContent) {
+    case "STREET'ZZA":
+      infoBoite.textContent =
+        " Un projet fait au cour de mon parcours au Cegep de Trois-Riviere.";
+      break;
+
+    case "INTERSTELLAR":
+      infoBoite.textContent =
+        " Un projet fait au cour de mon parcours au Cegep de Trois-Riviere2.";
+      break;
+
+    case "ELDEN RING":
+      infoBoite.textContent =
+        " Un projet fait au cour de mon parcours au Cegep d'Édouard-MontPetit.";
+      break;
+
+    case "CONCRETE WAVES":
+      infoBoite.textContent = " Un projet fait dans mon temps libre.";
+      break;
+
+    case "COSMOFEST":
+      infoBoite.textContent = " Un projet fait dans mon temps libre2.";
+      break;
+
+    case "LEVEL UP":
+      infoBoite.textContent =
+        " Un projet fait au cour de mon parcours au Cegep de Trois-Riviere3.";
+      break;
+  }
+ 
   
 });
 
-/*
-
-const activeItem = carousel.querySelector(
-    "[data-carousel-item]:not(.hidden)"
-  );
-  const box = activeItem.querySelector(".carousel_InfoBoite");
-
-*/
+btnFermer.addEventListener("click", () => {
+  projetsInfoPlus.classList.add("hidden");
+});
